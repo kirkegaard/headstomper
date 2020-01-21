c143998a6a4db27c6b1cff796765d9c7cd5f690b
Christian Kirkegaard <christian@lowpoly.dk>
2020-01-21 18:39:14 +0100

    bump

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Header from './components/Header.js';
+import './App.scss';
+
+const App = () => {
+  return (
+    <div className="app">
+      <Header />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Header.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Header.scss';
+
+const Header = () => {
+  return (
+    <header>
+      <div className="container">
+        <h1>Headstomper</h1>
+      </div>
+    </header>
+  );
+}
+
+export default Header;
