b68935d64138297a632240b00622daf5698ba2f4
Christian Kirkegaard <christian@lowpoly.dk>
2020-02-26 21:09:20 +0100

    flaf

--- src/components/Header.js
+++ src/components/Header.js
@@ -13,7 +13,7 @@ const Header = () => {
       <nav className="mb-auto">&nbsp;</nav>
       <div className="main mx-auto text-center mb-auto">
         <img className="logo img-fluid" src={logo} alt="Headstomper logo" />
-        <h2 className="subtitle">8-10 May 2020</h2>
+        <h2 className="subtitle">May 8-10, 2020</h2>
         <a href="https://smash.gg/tournament/headstomper-2020/register" className="btn btn-primary btn-lg">
           Register
         </a>
