e73e96a43e6317ea0ac2b169a3f515a2b2371601
Christian Kirkegaard <christian@lowpoly.dk>
2020-02-26 20:52:31 +0100

    vid

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,15 @@
 import React from "react";
+import poster from "../assets/images/header.jpg";
+import video from "../assets/videos/header-stomper.mp4";
 import logo from "../assets/logos/headstomper.png";
 
 const Header = () => {
   return (
     <header className="d-flex w-100 h-100 mx-auto flex-column">
-      <div className="wallpaper"></div>
+      <div className="fullscreen">
+        <video loop muted autoPlay poster={poster} src={video} className="video" />
+      </div>
+      <div className="fullscreen"></div>
       <nav className="mb-auto">&nbsp;</nav>
       <div className="main mx-auto text-center mb-auto">
         <img className="logo img-fluid" src={logo} alt="Headstomper logo" />
