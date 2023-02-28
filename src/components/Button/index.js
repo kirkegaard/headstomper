3c427c54aab4691ed6426f3c46eb90ea3cf4bbfe
Christian Kirkegaard <christian@lowpoly.dk>
2023-02-28 20:33:17 +0100

    Responsive headlines

--- src/components/Button/index.js
+++ src/components/Button/index.js
@@ -1,5 +1,5 @@
 import cx from "classnames";
-import styles from "./index.module.css";
+import styles from "./index.module.scss";
 
 const Button = ({ children, size, variant = "default" }) => {
   return (
