709c3de56b2eef923b0b73f66b7f83c66c8c67da
Christian Kirkegaard <christian@lowpoly.dk>
2025-01-21 09:40:42 +0100

    Add analytics

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,11 @@
+import { Analytics } from "@vercel/analytics/react";
 import "@/styles/globals.scss";
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Component {...pageProps} />
+      <Analytics />
+    </>
+  );
 }
